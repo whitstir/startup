a7a9a181d5c931e996c546ec45703ebfb7bc698c
Whitney <whitstirling@gmail.com>
2025-10-08 15:34:05 -0600

    Made all my little jsx files!

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="main-body">
+    <header>
+        <h1 id="commonly-logo">Commonly</h1>
+        <nav class="nav-bar">
+
+            <ul>
+                <li>
+                    <a href="progress_tracker.html">Progress Tracker</a>
+                </li>
+                <li>
+                    <a href="calendar.html">Calendar</a>
+                </li>
+                <li>
+                    <a href="mytasks.html">My Tasks</a>
+                </li>
+                <li>
+                    <a href="index.html">Login</a>
+                </li>
+            </ul>
+            <h3>Username displayed here</h3>
+        </nav>
+    </header>
+    <div className="body bg-dark text-light">App will display here</div>
+    <footer>
+    <div>
+        <h2>Database Placeholder</h2>
+        <p>The database will save and remember your chores.</p>
+        <h2>Web Socket Placeholder</h2>
+        <p>The Web Socket of this website will allow for collaboration on household chores by communicating with other
+            devices,
+            regularly updating the progress task bar based on others' work. </p>
+    </div>
+    Commonly (Footer Placeholder)
+</footer>
+</div>
+    );
 }
