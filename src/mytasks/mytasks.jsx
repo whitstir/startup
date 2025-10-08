acf2cd2bfebddd8a09c31b450bd0a1da24fa2530
Whitney <whitstirling@gmail.com>
2025-10-08 17:42:19 -0600

    Moved the main html in mytasks to the .jsx

--- src/mytasks/mytasks.jsx
+++ src/mytasks/mytasks.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 export function MyTasks() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>mytasks displayed here</div>
+    <main>
+        <div className="white-box">
+            <h1>My Tasks</h1>
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+                <li>Task 4</li>
+                <li>Task 5</li>
+            </ul>
+        </div>
     </main>
   );
 }
