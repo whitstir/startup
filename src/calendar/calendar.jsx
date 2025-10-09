d12b4f9538afcf5689339e72a69411c59997981c
Whitney <whitstirling@gmail.com>
2025-10-08 18:04:58 -0600

    Put the calendar html into the jsx

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export function Calendar() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Calendar displayed here</div>
+    <main>
+        <div class="white-box">
+
+            <h1>Calendar</h1>
+            <p>3rd Party API Placeholder for calendar</p>
+            <img src="towfiqu-barbhuiya-bwOAixLG0uc-unsplash.jpg" height={300}/>
+
+        </div>
     </main>
   );
 }
