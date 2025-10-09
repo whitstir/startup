db8548374a78b3269ab99f98a1bf8797b3eb0d78
Whitney <whitstirling@gmail.com>
2025-10-08 18:33:12 -0600

    added login html to jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Login() {
-  return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="white-box">
+                <h1>Login</h1>
+                <form>
+                    <label htmlFor="first">Username:</label>
+                    <input type="text" id="first" name="first" placeholder="Username or Email" required />
+
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" placeholder="Password" required />
+
+                </form>
+                <NavLink to="/progress_tracker">
+                    <button className="button">Login</button>
+                </NavLink>
+                <div>
+                    <a href="https://github.com/whitstir/startup.git">
+                        <button className="button">Look at my GitHub!</button>
+                    </a>
+                </div>
+            </div>
+        </main>
+    );
 }
