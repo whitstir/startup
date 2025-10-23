2e4507e3c41734339c49f03d1c99506291c2d4bd
Whitney <whitstirling@gmail.com>
2025-10-22 21:06:28 -0600

    Added functinality to the roommates part

--- src/roommates/roommates.jsx
+++ src/roommates/roommates.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function Roommates() {
+  const [roommates] = useState([
+    { name: 'Whitney', progress: '80%'},
+    { name: 'Allie', progress: '50%'},
+    { name: 'Blair', progress: '65%'},
+    { name: 'Lydia', progress: '15%'},
+    { name: 'Victoria', progress: '30%'},
+  ]);
+
   return (
     <main>
         <div className="white-box">
             <h1>Roomate Progress Tracker</h1>
+            <p>Check how your roommates are doing with their chores:</p>
             <ul>
-                <li>Roommate 1 - Progress</li>
-                <li>Roommate 2 - Progress</li>
-                <li>Roommate 3 - Progress</li>
-                <li>Roommate 4 - Progress</li>
-                <li>Roommate 5 - Progress</li>
+                {roommates.map((roommate, index) => (
+                  <li key={index}>
+                    {roommate.name} - {roommate.progress} complete
+                  </li>
+                ))}
             </ul>
         </div>
     </main>
