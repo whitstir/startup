30722fa51689b0279a2824db85eafe03397c522e
Whitney <whitstirling@gmail.com>
2025-11-10 23:54:08 -0700

    Realized that my startup wasn't connecting to Mongo because it was going into the simon file instead of the startup... so that's fixed now

--- service/database.js
+++ service/database.js
@@ -3,10 +3,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
-const recipeCollection = db.collection('recipe');
 
 (async function testConnection() {
   try {
