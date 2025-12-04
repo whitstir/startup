ac68c0c3b34c69bc9fae53736d49977496be8570
Whitney <whitstirling@gmail.com>
2025-12-03 21:11:18 -0700

    Added the code from the example

---
+++ service/service.js
@@ -0,0 +1,42 @@
+const { WebSocketServer } = require('ws');
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+server = app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
+
+// Create a websocket object
+const socketServer = new WebSocketServer({ server });
+
+socketServer.on('connection', (socket) => {
+  socket.isAlive = true;
+
+  // Forward messages to everyone except the sender
+  socket.on('message', function message(data) {
+    socketServer.clients.forEach(function each(client) {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        console.log('forwarding message to client');
+        client.send(data);
+      }
+    });
+  });
+
+  // Respond to pong messages by marking the connection alive
+  socket.on('pong', () => {
+    console.log('recieved pong from client')
+    socket.isAlive = true;
+  });
+});
+
+// Periodically send out a ping message to make sure clients are alive
+setInterval(() => {
+  socketServer.clients.forEach(function each(client) {
+    if (client.isAlive === false) return client.terminate();
+
+    client.isAlive = false;
+    console.log('sending ping to client')
+    client.ping();
+  });
+}, 10000);
